12dc1b5eed334ef3ec940c4ef8337ee9421266c1
Tristan Francis <tristan@MacBookAir.Dlink>
2025-05-28 06:32:54 +0200

    add vercel analytics update

--- app/layout.js
+++ app/layout.js
@@ -30,6 +30,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ClerkProvider>
+          <Analytics/>
           <Navbar />
           {children}
           <Footer />
